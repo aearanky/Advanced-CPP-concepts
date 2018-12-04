177eb0817f5841c085e24cf0c552ebe419542d1a
Aditya Earanky <aearanky@terpmail.umd.edu>
2018-12-03 21:54:16 -0500

    Updated multiple_inheritance.cpp with an advanced example

--- Inheritance/multiple_inheritance.cpp
+++ Inheritance/multiple_inheritance.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 
@@ -10,6 +11,14 @@ struct A {
         b = j;
         cout<<"A's constructor called"<<endl;
     }
+    
+    ~A() {
+        cout << "A's destructor called"<<endl;
+    }
+    
+    int get_a() {
+        return a;
+    }
 };
 
 struct B {
@@ -20,26 +29,63 @@ struct B {
         d = j;
         cout<<"B's constructor called"<<endl;
     }
+    
+    ~B() {
+        cout << "B's destructor called"<<endl;
+    }
 };
 
-struct derived_C : A, B {
+struct C : A, B {
     int e, f;
-    derived_C(int p, int q, string str, int t, int u, int v ) : A (p, q), B (str, t) {
+    C(int p, int q, string str, int t, int u, int v ) : A (p, q), B (str, t) {
         e = u;
         f = v;
-        cout<<"derived_C's constructor called"<<endl;
+        cout<<"C's constructor called"<<endl;
     }
-}; 
+    
+    ~C() {
+        cout << "C's destructor called"<<endl;
+    }
+};
+
+struct D : A {
+    
+    A* an_A_obj_pointer;
+    
+    D(int i, int j, A* some_pointer_to_A): A(i,j) {
+        an_A_obj_pointer = some_pointer_to_A;
+        cout << "D's constructor called"<<endl;
+    }
+    
+    ~D() {
+        cout << "D's destructor called"<<endl;
+    }
+};
 
 int main()
 {
-    derived_C myObj(1, 2, "yo mister", 4, 5, 6);
-    cout<<myObj.a << " " << myObj.b << " " <<myObj.c << " " << myObj.d << " " << myObj.e << " " << myObj.f << " ";
+    C myObj(1, 2, "yo mister", 4, 5, 6);
+    cout<<myObj.a << " " << myObj.b << " " <<myObj.c << " " << myObj.d << " " << myObj.e << " " << myObj.f << " " <<endl;
+    
+    D myDobj(7,8, new A(7,8));
+    cout<<myDobj.an_A_obj_pointer->get_a() << " " << myDobj.an_A_obj_pointer->b<<endl;
+    
     return 0;
 }
 
-/* Expected output:
+/*Expected Output:
 A's constructor called                                                                                                            
 B's constructor called                                                                                                            
-derived_C's constructor called                                                                                                    
-1 2 baby 4 5 6  */
+C's constructor called                                                                                                            
+1 2 yo mister 4 5 6                                                                                                               
+A's constructor called                                                                                                            
+A's constructor called                                                                                                            
+D's constructor called                                                                                                            
+7 8
+
+D's destructor called                                                                                                             
+A's destructor called                                                                                                             
+C's destructor called                                                                                                             
+B's destructor called                                                                                                             
+A's destructor called 
+*/
